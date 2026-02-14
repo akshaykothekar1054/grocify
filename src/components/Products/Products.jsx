07d8968c0caee5bc1545023ef3d02108d37bc21f
akshay <akshayk@promaxevents.in>
2026-02-14 14:56:26 +0530

    Added grocify name is Products section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,7 +4,7 @@ const Products = () => {
     return (
         <section>
             <div className='max-w-[1400px] mx-auto px-10 py-20'>
-                <Heading highlight="Our" content="Products" />
+                <Heading highlight="Our" content="Products in Grocify" />
 
             </div>
         </section>
