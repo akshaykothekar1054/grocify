6d376eb6f56d3925d25ee923134b361583d776fd
akshay <akshayk@promaxevents.in>
2026-02-14 14:58:11 +0530

    remove grocify name from products page

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,7 +4,7 @@ const Products = () => {
     return (
         <section>
             <div className='max-w-[1400px] mx-auto px-10 py-20'>
-                <Heading highlight="Our" content="Products in Grocify" />
+                <Heading highlight="Our" content="Products" />
 
             </div>
         </section>
